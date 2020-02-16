01417f56dadc313f7f719d88a85ea956f3efb98a
Ryohji Ikebe <Ryohji.Ikebe@gmail.com>
2020-02-16 14:04:42 +0900

    Update thread expansion implementation.

--- ctl/src/thread/model.kt
+++ ctl/src/thread/model.kt
@@ -1,39 +1,48 @@
 package thread
 
-interface Shared
+data class SystemState(val shared: SharedVar, val threadStates: List<ThreadState>)
+data class ThreadState(val location: Location, val variable: LocalVar)
 
-interface ThreadLocal {
-    val ir: Location
-}
+infix fun SharedVar.with(threadStates: List<ThreadState>) = SystemState(this, threadStates)
+infix fun LocalVar.on(location: Location) = ThreadState(location, this)
+infix fun SharedVar.with(local: LocalVar) = Transition.Variable(this, local)
 
+interface SharedVar
+interface LocalVar
 interface Location
 
-data class Thread(val shared: Shared, val local: ThreadLocal)
+data class Transition(val name: String, val arrow: Pair<Location, Location>, val apply: (Variable) -> Variable?) {
+    data class Variable(val shared: SharedVar, val local: LocalVar)
 
-data class State(val shared: Shared, val locals: List<ThreadLocal>)
-
-data class Link(val name: String, val from: State, val to: State)
-
-interface Rule {
-    val name: String
-    fun applicable(t: Thread): Boolean
-    fun convert(t: Thread): Thread
+    val location get() = arrow.first
+    val boundTo get() = arrow.second
 }
 
-fun buildGraph(rules: List<Rule>, initialState: State): Set<Link> = mutableSetOf<Link>().apply {
-    val frontier = mutableSetOf(initialState)
+interface Edge {
+    val state: SystemState
+}
+
+data class Node(override val state: SystemState) : Edge
+data class Link(override val state: SystemState, val boundTo: SystemState) : Edge
+
+fun buildGraph(transitions: List<Transition>, initial: SystemState): Set<Edge> = mutableSetOf<Edge>().also { graph ->
+    val frontier = mutableSetOf(initial)
     while (frontier.isNotEmpty()) {
-        val from = frontier.first().also { frontier.remove(it) } // pop front
-        val next = from.locals.mapIndexed { index, local ->
-            rules.mapNotNull { it.apply(Thread(from.shared, local)) }.map { (name, thread) ->
-                State(thread.shared, swapAt(index, from.locals, thread.local))
-                    .also { this += Link(name, from, it) } // 次の状態を算出するとともにグラフにリンクを追加
+        frontier.first().let { state ->
+            state.threadStates.mapIndexed { i, t ->
+                transitions.filter { t.location == it.location }.mapNotNull { transition ->
+                    with(transition) {
+                        apply(state.shared with t.variable)?.let {
+                            it.shared with state.threadStates.alteredAt(i, it.local on boundTo)
+                        }
+                    }
+                }
+            }.flatten().also { states ->
+                graph.addAll(if (states.isEmpty()) listOf(Node(state)) else states.map { Link(state, it) })
+                frontier.apply { addAll(states); removeAll(graph.map { it.state }) }
             }
-        }.flatten()
-        this.map(Link::from).toSet().let { visited -> with(frontier) { addAll(next); removeAll(visited) } }
+        }
     }
 }
 
-private fun Rule.apply(t: Thread): Pair<String, Thread>? = if (applicable(t)) name to convert(t) else null
-
-private fun <T> swapAt(n: Int, list: List<T>, item: T): List<T> = list.take(n) + item + list.drop(n + 1)
+private fun <T> List<T>.alteredAt(i: Int, x: T): List<T> = take(i) + x + drop(i + 1)
--- src/app.kt
+++ src/app.kt
@@ -2,46 +2,21 @@ import Location.*
 import ctl.*
 import thread.*
 
-fun main() = buildGraph(rules, thread.State(Shared(0, 0, 0), listOf(P0.toThreadLocal())))
+fun main() = buildGraph(trans, Variable(0, 0, 0) with listOf(object : LocalVar {} on P0))
     .joinToString("\n")
     .let { println(it) }
 
 enum class Location : thread.Location { P0, P1, P2, P3, P4 }
 
-data class Shared(val x: Int, val y: Int, val z: Int) : thread.Shared
+data class Variable(val x: Int, val y: Int, val z: Int) : SharedVar
 
-fun Location.toThreadLocal() = this.let { location ->
-    object : ThreadLocal {
-        override fun toString() = location.name
-        override val ir = location
-    }
-}
-
-fun Shared.xAltered(x: Int) = Shared(x, y, z)
-fun Shared.yAltered(y: Int) = Shared(x, y, z)
-fun Shared.zAltered(z: Int) = Shared(x, y, z)
-fun Thread.alter(location: Location, convert: (Shared) -> Shared): Thread =
-    Thread(convert(shared as Shared), location.toThreadLocal())
-
-private val rules = listOf(
-    object : Rule {
-        override val name = "x=1"
-        override fun applicable(t: Thread) = t.local.ir == P0
-        override fun convert(t: Thread) = t.alter(P1) { it.xAltered(1) }
-    },
-    object : Rule {
-        override val name = "y=1"
-        override fun applicable(t: Thread) = t.local.ir == P1
-        override fun convert(t: Thread) =  t.alter(P2) { it.yAltered(1) }
-    },
-    object : Rule {
-        override val name = "z=1"
-        override fun applicable(t: Thread) = t.local.ir == P2
-        override fun convert(t: Thread) = t.alter(P3) { it.zAltered(1) }
-    },
-    object : Rule {
-        override val name = "y=0"
-        override fun applicable(t: Thread) = t.local.ir == P3
-        override fun convert(t: Thread) =  t.alter(P4) { it.yAltered(0) }
-    }
+private val trans = listOf(
+    Transition("x=1", P0 to P1) { it.shared.xAltered(1) with it.local },
+    Transition("y=1", P1 to P2) { it.shared.yAltered(1) with it.local },
+    Transition("z=1", P2 to P3) { it.shared.zAltered(1) with it.local },
+    Transition("y=0", P3 to P4) { it.shared.yAltered(0) with it.local }
 )
+
+private fun SharedVar.xAltered(x: Int) = with(this as Variable) { Variable(x, y, z) }
+private fun SharedVar.yAltered(y: Int) = with(this as Variable) { Variable(x, y, z) }
+private fun SharedVar.zAltered(z: Int) = with(this as Variable) { Variable(x, y, z) }
