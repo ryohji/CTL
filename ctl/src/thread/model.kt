c46f4ed3d771904b3850bbf8219e21447aae7674
Ryohji Ikebe <Ryohji.Ikebe@gmail.com>
2020-02-16 21:58:54 +0900

    Replace `interface SharedVer` by type parameter.

--- ctl/src/thread/model.kt
+++ ctl/src/thread/model.kt
@@ -1,31 +1,30 @@
 package thread
 
-data class SystemState(val shared: SharedVar, val threadStates: List<ThreadState>)
+data class SystemState<SharedVar>(val shared: SharedVar, val threadStates: List<ThreadState>)
 data class ThreadState(val location: Location, val variable: LocalVar)
 
-infix fun SharedVar.with(threadStates: List<ThreadState>) = SystemState(this, threadStates)
+infix fun <SharedVar> SharedVar.with(threadStates: List<ThreadState>) = SystemState(this, threadStates)
 infix fun LocalVar.on(location: Location) = ThreadState(location, this)
-infix fun SharedVar.with(local: LocalVar) = Transition.Variable(this, local)
+infix fun <SharedVar> SharedVar.with(local: LocalVar) = Transition.Variable(this, local)
 
-interface SharedVar
 interface LocalVar
 interface Location
 
-data class Transition(val name: String, val arrow: Pair<Location, Location>, val apply: (Variable) -> Variable?) {
-    data class Variable(val shared: SharedVar, val local: LocalVar)
+data class Transition<SharedVar>(val name: String, val arrow: Pair<Location, Location>, val apply: (Variable<SharedVar>) -> Variable<SharedVar>?) {
+    data class Variable<SharedVar>(val shared: SharedVar, val local: LocalVar)
 
     val location get() = arrow.first
     val boundTo get() = arrow.second
 }
 
-interface Edge {
-    val state: SystemState
+interface Edge<SharedVar> {
+    val state: SystemState<SharedVar>
 }
 
-data class Node(override val state: SystemState) : Edge
-data class Link(override val state: SystemState, val boundTo: SystemState) : Edge
+data class Node<SharedVar>(override val state: SystemState<SharedVar>) : Edge<SharedVar>
+data class Link<SharedVar>(override val state: SystemState<SharedVar>, val boundTo: SystemState<SharedVar>) : Edge<SharedVar>
 
-fun buildGraph(transitions: List<Transition>, initial: SystemState): Set<Edge> = mutableSetOf<Edge>().also { graph ->
+fun <SharedVar> buildGraph(transitions: List<Transition<SharedVar>>, initial: SystemState<SharedVar>): Set<Edge<SharedVar>> = mutableSetOf<Edge<SharedVar>>().also { graph ->
     val frontier = mutableSetOf(initial)
     while (frontier.isNotEmpty()) {
         frontier.first().let { state ->
--- src/app.kt
+++ src/app.kt
@@ -2,12 +2,12 @@ import Location.*
 import ctl.*
 import thread.*
 
-fun main() = buildGraph(trans, Variable(0, 0, 0) with listOf(object : LocalVar {} on P0))
+fun main() = buildGraph(trans, SharedVar(0, 0, 0) with listOf(object : LocalVar {} on P0))
     .let { graph ->
         mark(("x=1" and "y>0") or not("z=0") where listOf(
-            "x=1" denote { it.variable.x == 1 },
-            "y>0" denote { it.variable.y > 0 },
-            "z=0" denote { it.variable.z == 0 }
+            "x=1" denote { it.shared.x == 1 },
+            "y>0" denote { it.shared.y > 0 },
+            "z=0" denote { it.shared.z == 0 }
         ), convert(graph))
     }
     .joinToString("\n")
@@ -15,22 +15,20 @@ fun main() = buildGraph(trans, Variable(0, 0, 0) with listOf(object : LocalVar {
 
 enum class Location : thread.Location { P0, P1, P2, P3, P4 }
 
-data class Variable(val x: Int, val y: Int, val z: Int) : SharedVar
+data class SharedVar(val x: Int, val y: Int, val z: Int)
 
-private val trans = listOf(
+private val trans = listOf<Transition<SharedVar>>(
     Transition("x=1", P0 to P1) { it.shared.xAltered(1) with it.local },
     Transition("y=1", P1 to P2) { it.shared.yAltered(1) with it.local },
     Transition("z=1", P2 to P3) { it.shared.zAltered(1) with it.local },
     Transition("y=0", P3 to P4) { it.shared.yAltered(0) with it.local }
 )
 
-private fun SharedVar.xAltered(x: Int) = with(this as Variable) { Variable(x, y, z) }
-private fun SharedVar.yAltered(y: Int) = with(this as Variable) { Variable(x, y, z) }
-private fun SharedVar.zAltered(z: Int) = with(this as Variable) { Variable(x, y, z) }
+private fun SharedVar.xAltered(x: Int) = SharedVar(x, y, z)
+private fun SharedVar.yAltered(y: Int) = SharedVar(x, y, z)
+private fun SharedVar.zAltered(z: Int) = SharedVar(x, y, z)
 
-private fun convert(graph: Collection<Edge>): Graph<SystemState> = object : Graph<SystemState> {
+private fun convert(graph: Collection<Edge<SharedVar>>): Graph<SystemState<SharedVar>> = object : Graph<SystemState<SharedVar>> {
     override val node = graph.map { it.state }
-    override val edge = graph.filterIsInstance<Link>().map { Graph.Edge(it.state, it.boundTo) }
+    override val edge = graph.filterIsInstance<Link<SharedVar>>().map { Graph.Edge(it.state, it.boundTo) }
 }
-
-private val SystemState.variable get() = shared as Variable
