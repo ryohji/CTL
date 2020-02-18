6dff54d9a2020b283a9f1e34d9ddd4163c53a46a
Ryohji Ikebe <Ryohji.Ikebe@gmail.com>
2020-02-18 11:52:21 +0900

    Draw label on states.

--- src/app.kt
+++ src/app.kt
@@ -9,15 +9,13 @@ import thread.on
 import thread.with
 
 fun main() = (SharedVar(0, 0, 0) with listOf(Nil on P0)).let { initial ->
-    buildGraph(trans, initial)
-        .also { println(buildDot(it, initial)) }
-        .mark(("x=1" and "y>0") or not("z=0") where listOf(
-            "x=1" denote { it.shared.x == 1 },
-            "y>0" denote { it.shared.y > 0 },
-            "z=0" denote { it.shared.z == 0 }
-        ))
-        .joinToString("\n")
-        .let { println(it) }
+    val graph = buildGraph(trans, initial)
+    val label = graph.mark(("x=1" and "y>0") or not("z=0") where listOf(
+        "x=1" denote { it.shared.x == 1 },
+        "y>0" denote { it.shared.y > 0 },
+        "z=0" denote { it.shared.z == 0 }
+    ))
+    println(buildDot(graph, initial, label))
 }
 
 enum class Location : thread.Location { P0, P1, P2, P3, P4 }
@@ -41,16 +39,29 @@ private fun SharedVar.xAltered(x: Int) = SharedVar(x, y, z)
 private fun SharedVar.yAltered(y: Int) = SharedVar(x, y, z)
 private fun SharedVar.zAltered(z: Int) = SharedVar(x, y, z)
 
-fun <Node> buildDot(graph: Graph<Node>, initial: Node): String {
+fun <Node> buildDot(graph: Graph<Node>, initial: Node, label: List<Label<Node>>): String {
     val states = graph.map(Edge<Node>::node).toSet()
-    val nodes = states.mapIndexed { index, node ->
-        // TODO: "filled" でラベルつきノードを描画
-        "$index [label=\"$node\" style=solid]"
-    }.plus("s [label=start shape=none]").joinToString(";\n  ")
+    val nodes = states.mapIndexed { i, node -> "$i [label=\"$node\"]" }
     val edges = graph.filterIsInstance<Link<Node>>().map {
-        val start = states.indexOf(it.node)
-        val end = states.indexOf(it.boundTo)
-        "$start -> $end [label=\"${it.name}\"]"
-    }.plus("s -> ${states.indexOf(initial)}").joinToString(";\n  ")
-    return "digraph {\n  $nodes;\n  $edges;\n}"
+        with(it) { "${states.indexOf(node)} -> ${states.indexOf(boundTo)} [label=\"${name}\"]" }
+    }
+    val labels = states
+        .mapIndexed { i, state ->
+            i to label.filter { state == it.state }.map(Label<Node>::proposition)
+        }
+        .filter { it.second.isNotEmpty() }
+        .mapIndexed { i, it ->
+            "l$i [label=\"${it.second.joinToString(",\\n")}\"]" to "l$i -> ${it.first}"
+        }
+        .unzip()
+    return "digraph {\n" +
+            "  s [shape=point];\n" +
+            "  s -> ${states.indexOf(initial)};\n" +
+            "  ${nodes.joinToString(";\n  ")};\n" +
+            "  ${edges.joinToString(";\n  ")};\n" +
+            "  node [shape=none];\n" +
+            "  ${labels.first.joinToString(";\n  ")};\n" +
+            "  edge [arrowhead=none];\n" +
+            "  ${labels.second.joinToString(";\n  ")};\n" +
+            "}"
 }
